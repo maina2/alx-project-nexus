a45349c9400ae03dffa9345d786e62dcf6f43e53
Simon Chanzu <chanzusimon6@gmail.com>
2025-07-31 10:38:14 +0300

    commits

--- pollpro_backend/asgi.py
+++ pollpro_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for poll_system_backend project.
+ASGI config for pollpro_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll_system_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollpro_backend.settings')
 
 application = get_asgi_application()
--- pollpro_backend/wsgi.py
+++ pollpro_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for poll_system_backend project.
+WSGI config for pollpro_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll_system_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollpro_backend.settings')
 
 application = get_wsgi_application()
